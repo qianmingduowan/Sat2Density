a286c9587cdb4bdac0f0fec7c1e2b920b051bc3b
Qian Ming <mingqian@whu.edu.cn>
2023-12-13 15:45:42 +0800

    Update download_weights.sh

--- scripts/download_weights.sh
+++ scripts/download_weights.sh
@@ -1,4 +1,4 @@
-CHECKPOINTS="run-20230303_142752-2cqv8uh4.zip run-20230219_141512-2u87bj8w.zip"
+CHECKPOINTS="cvusa/run-20230303_142752-2cqv8uh4.zip cvact/run-20230219_141512-2u87bj8w.zip"
 
 if [ ! -d "wandb" ]; then
   mkdir wandb
@@ -7,7 +7,7 @@ fi
 for checkpoint in $CHECKPOINTS ; do
     echo "Downloading $checkpoint";
     if [ ! -f "wandb/$checkpoint" ]; then
-        wget https://github.com/sat2density/checkpoints/releases/download/cvusa/$checkpoint -P wandb
+        wget https://github.com/sat2density/checkpoints/releases/download/$checkpoint -P wandb
     fi
     echo "Unzipping $checkpoint";
     if [ ! -d "wandb/${checkpoint%.*}" ]; then
